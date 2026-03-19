eb781974614d6c798a3e049e0f56be55b23772f1
Matúš Gromnica <matus.gromnica779@student.cuni.cz>
2026-03-19 23:28:22 +0100

    Okno sa hybe??

--- sidequest/Sidequest/Sidequest/MainWindow.xaml.cs
+++ sidequest/Sidequest/Sidequest/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,15 +19,64 @@ namespace Sidequest
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool isAnimating = false;
+        public bool isExpanded = false;
+
+        private double _anchorRight;
+        private double _anchorBottom;
+
         public MainWindow()
         {
             InitializeComponent();
 
             var desktopWorkingArea = SystemParameters.WorkArea;
 
-            this.Left = desktopWorkingArea.Right - this.Width - 20;
+            this.Width = 40;
+            this.Height = 40;
 
-            this.Top = desktopWorkingArea.Bottom - this.Height - 20;
+            _anchorRight = desktopWorkingArea.Right - 20;
+            _anchorBottom = desktopWorkingArea.Bottom - 20;
+            
+
+            this.Left = _anchorRight - this.Width;
+            this.Top = _anchorBottom - this.Height;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Expand(object sender, MouseEventArgs e)
+        {
+            if (this.Width > 290 && this.Height > 290) return;
+            animateWindow(300);
+        }
+
+        private void Window_Collapse(object sender, MouseEventArgs e)
+        {
+            if (this.Width < 50 && this.Height < 50) return;
+            animateWindow(40);
+        }
+
+        private void animateProperty(DependencyProperty prop, double targetSize)
+        {
+            DoubleAnimation sizeAnim = new DoubleAnimation()
+            {
+                To = targetSize,
+                Duration = TimeSpan.FromMilliseconds(200),
+                EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut}
+            };
+            this.BeginAnimation(prop, sizeAnim, HandoffBehavior.Compose);
+        }
+
+        private void animateWindow(double targetSize)
+        {
+            animateProperty(Window.WidthProperty, targetSize);
+            animateProperty(Window.HeightProperty, targetSize);
+
+            animateProperty(Window.LeftProperty, _anchorRight - targetSize);
+            animateProperty(Window.TopProperty, _anchorBottom - targetSize);
         }
     }
 }
