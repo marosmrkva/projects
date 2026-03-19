07b2d0e2e6c270bd845a301c40654230f2c69f88
Matúš Gromnica <matus.gromnica779@student.cuni.cz>
2026-03-19 21:02:47 +0100

    Pomaly zaciatok

---
+++ sidequest/Sidequest/Sidequest/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sidequest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var desktopWorkingArea = SystemParameters.WorkArea;
+
+            this.Left = desktopWorkingArea.Right - this.Width - 20;
+
+            this.Top = desktopWorkingArea.Bottom - this.Height - 20;
+        }
+    }
+}
