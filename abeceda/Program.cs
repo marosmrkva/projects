439c52445bdd5a0408b3ee1e16a9a7e3b8d1c709
Matus Gromnica <matusgromnica@MacBook-Air-uzivatela-Matus.local>
2026-04-18 18:32:52 +0200

    Abeceda upload

---
+++ abeceda/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace abeceda;
+
+class Program
+{
+    public static int getTotalClicks()
+    {
+        int tableWidth = Console.Read();
+        int tableHeight = Console.Read();
+        
+        char[,] table = new char[tableWidth, tableHeight];
+
+        for (int i = 0; i < tableWidth; i++)
+        {
+            for (int j = 0; j < tableHeight; j++)
+            {
+                table[i, j] = Console.ReadKey().KeyChar;
+            }
+        }
+        
+        string inputString = Console.ReadLine();
+        
+        char currentChar = inputString[0];
+        
+        
+        
+        
+        
+        
+        return 0;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine(getTotalClicks());
+    }
+}
